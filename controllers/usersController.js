3c1d579f080fcb698341d8a66502c0e89659ae32
Yousef500 <Kazuya14725@gmail.com>
2022-04-04 23:50:03 +0300

    Finished creating hash

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,22 +1,44 @@
 const Router = require("Koa-router");
-const db = require("../services/mongoClientService").db("WeightConversion");
+const Users = require("../services/mongoClientService").db("WeightConversion").collection("Users");
 const userService = require("../services/userService");
 const bcrypt = require("bcrypt");
 
 const router = new Router();
 
 router.post("/api/users/register", async (ctx) => {
-    const user = ctx.request.body;
-    user.password = await bcrypt.hash(user.password, 10);
+    const body = ctx.request.body;
+    const user = await Users.findOne({username: body.username});
+    if (user) {
+        ctx.status = 400;
+        ctx.body = {
+            message: "Username already exists"
+        };
+    } else {
+        await bcrypt.hash(body.password, 10, async (hashErr, hash) => {
+            if (hashErr) {
+                ctx.status = 403;
+                return ctx.throw(403, hashErr.toString());
+            }
+            try {
+                await Users.insertOne({
+                    ...body,
+                    password: hash
+                });
+                ctx.status = 201;
+                ctx.body = "";
+            } catch (e) {
+                ctx.status = 403;
+                ctx.throw(403, e.toString());
+            }
+        });
 
+    }
 
 
     // await db.collection("Users").insertOne({
     //     ...user,
     //     password: hashedPassword
     // });
-
-    console.log(user);
 });
 
 router.get("/api/users", async (ctx) => {
