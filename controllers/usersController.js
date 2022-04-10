ab0cbde74d58d3b3fb3636d2969a92b54b6cf30b
Yousef500 <Kazuya14725@gmail.com>
2022-04-10 08:56:53 +0300

    reformatted code

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,37 +1,39 @@
 const Router = require("koa-router");
-const { registerUser, getUsers, getUserByUsername, loginUser } = require("../services/userService");
+const {registerUser, getUsers, getUserByUsername, loginUser} = require("../services/userService");
 
 const router = new Router();
 
 router.post("/api/users/register", async (ctx) => {
-  const body = ctx.request.body;
-  const user = await getUserByUsername(body.username);
-  if (user) {
-    ctx.status = 400;
-    ctx.body = {
-      message: "Username already exists"
-    };
-  } else {
-    try {
-      await registerUser(body);
-      ctx.status = 201;
-      return ctx.body = '';
-    } catch (err) {
-      ctx.status = 403;
-      return ctx.throw(403, e);
+    const body = ctx.request.body;
+    const user = await getUserByUsername(body.username);
+    if (user) {
+        ctx.status = 400;
+        ctx.body = {
+            message: "Username already exists"
+        };
+    } else {
+        try {
+            await registerUser(body);
+            ctx.status = 201;
+            return ctx.body = '';
+        } catch (err) {
+            ctx.status = 403;
+            return ctx.throw(403, err);
+        }
     }
-  }
 });
 
 router.post('/api/users/login', async (ctx) => {
-  const body = ctx.request.body;
-  const result = await loginUser(body.username, body.password);
-  ctx.status = result.code;
-  ctx.body = result.message;
+    const body = ctx.request.body;
+    const result = await loginUser(body.username, body.password);
+    ctx.status = result.code;
+    ctx.body = {
+        message: result.message
+    };
 });
 
 router.get("/api/users", async (ctx) => {
-  ctx.body = await getUsers();
+    ctx.body = await getUsers();
 });
 
 module.exports = router;
--- services/userService.js
+++ services/userService.js
@@ -1,42 +1,42 @@
 const bcrypt = require('bcrypt');
 const Users = require("../services/mongoClientService").db("WeightConversion").collection("Users");
 
-const registerUser = async (body) => {
-  await bcrypt.hash(body.password, 10, async (hashErr, hash) => {
-    if (hashErr) {
-      return hashErr
-    }
-    try {
-      await Users.insertOne({
-        ...body,
-        password: hash
-      });
-    } catch (e) {
-      return e;
-    }
-  });
+const registerUser = async (user) => {
+    await bcrypt.hash(user.password, 10, async (hashErr, hash) => {
+        if (hashErr) {
+            return hashErr
+        }
+        try {
+            await Users.insertOne({
+                ...user,
+                password: hash
+            });
+        } catch (e) {
+            return e;
+        }
+    });
 }
 
 const getUsers = async () => {
-  return await Users.find().toArray();
+    return await Users.find().toArray();
 }
 
 const getUserByUsername = async (username) => {
-  return await Users.findOne({ username });
+    return await Users.findOne({username});
 }
 
 const loginUser = async (username, password) => {
-  const user = await getUserByUsername(username);
-  if (!user) {
-    return { code: 404, message: "No such user exists" };
-  } else {
-    const match = await bcrypt.compare(password, user.password);
-    if (match) {
-      return { code: 200, message: "Logged in successfully" };
+    const user = await getUserByUsername(username);
+    if (!user) {
+        return {code: 404, message: "No such user exists"};
     } else {
-      return { code: 404, message: "Please check your credentials" };
+        const match = await bcrypt.compare(password, user.password);
+        if (match) {
+            return {code: 200, message: "Logged in successfully"};
+        } else {
+            return {code: 404, message: "Please check your credentials"};
+        }
     }
-  }
 }
 
-module.exports = { registerUser, getUsers, getUserByUsername, loginUser };
+module.exports = {registerUser, getUsers, getUserByUsername, loginUser};
